fd0d44c057d1fcdaa9797db0c20761d2d9858486
AndrewIsraelsen <andreweisraelsen@gmail.com>
2025-10-08 23:31:41 -0600

    converting to routing and nav setup (half way)

---
+++ src/stats/stats.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export function Stats() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>Stats displayed here</div>
+    </main>
+  );
+}
---
+++ src/timeline/login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export function Timeline() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>Timeline displayed here</div>
+    </main>
+  );
+}
